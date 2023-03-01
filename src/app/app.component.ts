6e2c85ff40940c18882066357b021310debc80aa
Matthew Lawrence <matt.lawrence@improving.com>
2023-03-01 14:08:06 -0500

    add eventlistner for message post.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'i-frame-checkout-test';
+
+  ngOnInit():void {
+    this.postListener()
+  }
+
+
+  postListener = () => {
+    console.log('yes')
+    // Listen for messages from the sending window
+    window.addEventListener('message', function(event) {
+      console.log(event)
+      // Check that the message came from the expected origin
+      if (event.origin === 'http://localhost:4200') {
+        // Do something with the message
+        console.log('Received message:', event.data);
+      }
+    });
+  }
+
 }
